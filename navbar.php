1233d9ce61b0015a177f56c085f19e5d6af7aee9
Nextz <102952730+nextz06@users.noreply.github.com>
2023-06-27 16:55:45 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <!-- Navbar start -->
 <nav class="navbar">
-    <a href="#" class="navbar-logo">
-        <img src="/img/logo.png" alt="Fakultas Teknologi Informasi Unuversitas Andalas">
+    <a href="/ux/index.php" class="navbar-logo">
+        <img src="/ux/img/logo.png" alt="Fakultas Teknologi Informasi Unuversitas Andalas">
     </a>
     <div class="navbar-nav">
         <ul>
@@ -15,7 +15,7 @@
         <ul>
             <li>
                 <div class="dropdown">
-                    <a href="index.php"><button class="dropbtn">Beranda</button></a>
+                    <a href="/ux/index.php"><button class="dropbtn">Beranda</button></a>
                 </div>
             </li>
             <li>
@@ -23,8 +23,8 @@
                     <button class="dropbtn">Profile
                     </button>
                     <div class="dropdown-content">
-                        <a href="/profile/profile.php#visi">Visi, Misi & Indikator</a>
-                        <a href="/profile/profile.php#struktur">Struktur Organisasi</a>
+                        <a href="/ux/profile/profile.php#visi">Visi, Misi & Indikator</a>
+                        <a href="/ux/profile/profile.php#struktur">Struktur Organisasi</a>
                         <a href="https://youtu.be/LFvGQX1waBk" target="_blank">Video Profil</a>
                     </div>
                 </div>
@@ -36,16 +36,16 @@
                     <div class="dropdown-content-2">
                         <div class="row">
                             <div class="column">
-                                <a href="/data/pegawai.php">Pegawai</a>
-                                <a href="/data/mahasiswa.php">Mahasiswa</a>
-                                <a href="/data/beasiswa.php">Beasiswa</a>
-                                <a href="/data/student-mobility.php">Student Mobility</a>
+                                <a href="/ux/data/pegawai.php">Pegawai</a>
+                                <a href="/ux/data/mahasiswa.php">Mahasiswa</a>
+                                <a href="/ux/data/beasiswa.php">Beasiswa</a>
+                                <a href="/ux/data/student-mobility.php">Student Mobility</a>
                             </div>
                             <div class="column">
-                                <a href="/data/workshop.php">Workshop</a>
-                                <a href="/data/kuliah-umum.php">Kuliah Umum</a>
-                                <a href="/data/AEE.php">AEE</a>
-                                <a href="/data/evaluasi.php">Evaluasi</a>
+                                <a href="/ux/data/workshop.php">Workshop</a>
+                                <a href="/ux/data/kuliah-umum.php">Kuliah Umum</a>
+                                <a href="/ux/data/AEE.php">AEE</a>
+                                <a href="/ux/data/evaluasi.php">Evaluasi</a>
                             </div>
                         </div>
                     </div>
@@ -56,7 +56,7 @@
                     <button class="dropbtn">Akademik
                     </button>
                     <div class="dropdown-content">
-                        <a href="/jadwalperkuliahan.php">Jadwal Perkuliahan</a>
+                        <a href="/ux/jadwalperkuliahan.php">Jadwal Perkuliahan</a>
                         <a href="http://ce.fti.unand.ac.id/">Teknik Komputer</a>
                         <a href="http://si.fti.unand.ac.id/">Sistem Informasi</a>
                         <a href="#">Informatika</a>
@@ -72,12 +72,12 @@
                     </button>
                     <div class="dropdown-content">
                         <a href="mailto:sekretariat@it.unand.ac.id">Hubungi Kami</a>
-                        <a href="/bantuan/form.php">Form</a>
-                        <a href="/bantuan/skd.php">Surat Keputusan Dekan</a>
-                        <a href="/bantuan/lainnya.php">Dokumen Lainnya</a>
-                        <a href="/bantuan/syarat.php">Syarat Pengurusan Dokumen</a>
-                        <a href="/bantuan/sertif.php">Sertifikat Akreditasi</a>
-                        <a href="/bantuan/sop.php">SOP FTI</a>
+                        <a href="/ux/bantuan/form.php">Form</a>
+                        <a href="/ux/bantuan/skd.php">Surat Keputusan Dekan</a>
+                        <a href="/ux/bantuan/lainnya.php">Dokumen Lainnya</a>
+                        <a href="/ux/bantuan/syarat.php">Syarat Pengurusan Dokumen</a>
+                        <a href="/ux/bantuan/sertif.php">Sertifikat Akreditasi</a>
+                        <a href="/ux/bantuan/sop.php">SOP FTI</a>
                         <a href="https://drive.google.com/file/d/1mrgivfO-9_5wafvc3Gww0ag_B4IduuLW/view?usp=sharing">Pedoman
                             Akademik</a>
                     </div>
